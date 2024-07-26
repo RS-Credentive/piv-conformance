2be3f5368da725f8838b2291e5506be408682738
Robert Sherwood - GSA <88669393+rsherwood-gsa@users.noreply.github.com>
2024-07-26 23:46:00 +0000

    update module-info

--- cardlib/src/main/java/module-info.java
+++ cardlib/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module gov.gsa.pivconformance.cardlib {
     exports gov.gsa.pivconformance.cardlib.utils;
 
     requires org.apache.commons.codec;
-    requires org.apache.commons.cli;
+    requires commons.cli;
     requires org.slf4j;
     requires transitive java.smartcardio;
     requires transitive org.bouncycastle.pkix;
--- conformancelib/src/main/java/module-info.java
+++ conformancelib/src/main/java/module-info.java
@@ -1,4 +1,11 @@
 module gov.gsa.pifconformance.conformancelib {
     requires java.smartcardio;
+    requires java.sql;
     requires org.slf4j;
+    requires org.mybatis;
+    requires org.junit.jupiter.api;
+    requires org.junit.jupiter.params;
+    requires org.junit.platform.commons;
+    requires org.junit.platform.engine;
+    requires gov.gsa.pivconformance.cardlib;
 }
